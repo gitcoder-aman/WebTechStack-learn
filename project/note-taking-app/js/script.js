ec7ff5da379bb1a54b07ebf47fd1e88c48ceb226
gitcoder-aman <amankumar93578@gmail.com>
2026-07-23 22:49:44 +0530

    making note-taking-app

---
+++ project/note-taking-app/js/script.js
@@ -0,0 +1,56 @@
+const addbtn = document.querySelector("#addbtn");
+const main = document.querySelector("#main");
+
+const saveNotes = () => {
+  const notes = document.querySelectorAll(".note textarea");
+  console.log(notes);
+  const data = [];
+  notes.forEach((note) => data.push(note.value));
+  console.log(data);
+
+  if (data.length == 0) {
+    localStorage.removeItem("notes");
+  } else {
+    localStorage.setItem("notes", JSON.stringify(data));
+  }
+};
+addbtn.addEventListener("click", function () {
+  addNote();
+});
+
+const addNote = (text = "") => {
+  const note = document.createElement("div");
+  note.classList.add("note");
+  note.innerHTML = `
+            <div class="tool">
+                <i class="fas fa-trash trash"></i>
+                <i class="fas fa-save save"></i>
+            </div>
+            <textarea>${text}</textarea>`;
+  note.querySelector(".trash").addEventListener("click", function () {
+    note.remove();
+    saveNotes();
+  });
+  note.querySelector(".save").addEventListener("click", function () {
+    saveNotes();
+  });
+  //   auto save when focus out
+  note.querySelector("textarea").addEventListener("focusout", function () {
+    saveNotes();
+  });
+  main.appendChild(note);
+  saveNotes();
+};
+
+// self calling function
+(function () {
+  const lsNotes = JSON.parse(localStorage.getItem("notes"));
+  if (lsNotes == null) {
+    addNote();
+  } else {
+    lsNotes.forEach((lsNote) => {
+      addNote(lsNote);
+    });
+  }
+  console.log(lsNotes);
+})();
